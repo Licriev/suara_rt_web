5d1a8a700e88f3e93aa5ce46771ad92a45d246c9
Sayuti Achmad <30473902+embowth@users.noreply.github.com>
2018-02-15 15:53:46 +0700

    surat
    surat

---
+++ surat.php
@@ -0,0 +1,77 @@
+<html> 
+<head>
+<title>Surat Pengantar</title>
+</head>
+
+<body>
+<table align="center" border="0" cellpadding="1" style="width: 700px;"><tbody>
+<tr>     <td colspan="3"><div align="center">
+<span style="font-family: Arial; font-size: x-small;"><b>PEMERINTAH KOTA BEKASI</b></span>
+<br>
+<span style="font-family: Arial; font-size: x-small;"><b>KECAMATAN BEKASI BARAT - KELURAHAN BINTARA JAYA</b></span>
+<br>
+<span style="font-family: Arial; font-size: x-small;"><b>RT 06 RW 09</b></span>
+<hr />
+</div>
+</td>   </tr>
+	<tr>     
+	<td colspan="2"><table align="center" border="0" cellpadding="1" style="width: 400px;"><tbody></td>      
+	</tr>
+
+<div align="center">
+<span style="font-size: x-small;">
+
+SURAT KETERANGAN PENGANTAR <br>
+Nomor : 212/SKD/VI/2017
+</span> 
+</div>
+<br>
+
+<tr>     
+	<td colspan="3" height="270" valign="top"><div align="justify">
+		<pre><span style="font-size: x-small;">Yang bertanda tangan dibawah ini Ketua RT. 06 RW. 09 Bintara Jaya Kelurahan Bintara Jaya  Kecamatan Bekasi Barat Kota Bekasi menerangkan dengan ini sebenarnya bahwa :</span>
+		</pre>
+<table border="0" style="width: 352px;"><tbody>
+			<tr>           
+			<td width="80"><span style="font-size: x-small;">Nama:</span></td>
+			</tr>
+
+			<tr>           
+			<td><span style="font-size: x-small;">Tempat Tanggal Lahir :</span>
+			</td>                              
+			</tr>
+
+			<tr>           
+			<td><span style="font-size: x-small;">Jenis Kelamin :</span></td>       
+			</tr>
+
+			<tr>           
+			<td><span style="font-size: x-small;">Agama :</span></td>                        
+			</tr>
+			<tr>
+			<td><span style="font-size: x-small;">Status Perkawinan :</span></td>                        
+			</tr>
+			<tr>           
+			<td><span style="font-size: x-small;">Kewarganegaraan :</span></td>                        
+			</tr>
+			<tr>           
+			<td><span style="font-size: x-small;">Alamat :</span></td>                        
+			</tr>
+
+			<tr>           
+			<td><span style="font-size: x-small;">Keperluan :</span></td>                        
+			</tr>
+</tbody></table>
+<br>
+<div align="justify">
+	<span style="font-size: x-small;">
+	Orang tersebut diatas, memang benar adalah warga RT. 06 RW. 09 Kelurahan Bintara Jaya Kecamatan Bekasi Barat Kota Bekasi, Demikian surat pengantar ini kami buat agar dapat dipergunakan sebagaimana mestinya dan penuh tanggung jawab.</span> 
+</div>
+</div>
+
+</td>   
+</tr>
+
+
+</tbody></table></body>
+</html>
